0570ba7050151bb7a0bcd23001479aac6ad7de13
Sajan Kc <sajan1996kc@gmail.com>
2022-03-22 14:13:41 +0545

    lab1 completed

--- src/lab1/IpAddressTest.java
+++ src/lab1/IpAddressTest.java
@@ -1,9 +1,29 @@
 package lab1;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class IpAddressTest {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	/*
+	 * Write a program to get the IP address of localhost and identify if the
+	 * address is IPV4 or IPV6.
+	 */
+
+	public static void main(String[] args) throws UnknownHostException {
+		InetAddress ip = InetAddress.getLocalHost();
+
+		System.out.println(ip.getHostAddress());
+
+		byte[] bytes = ip.getAddress();
+		if (bytes.length == 4) {
+			System.out.println(ip + " is IPv4 address.");
+		} else if (bytes.length == 16) {
+			System.out.println(ip + " is IPv6 address.");
+		} else {
+			System.out.println("Neither.");
+		}
+
 	}
 
 }
